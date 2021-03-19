7f7c561c1efb61f7d44e4540842ec53458f5acab
Jonas Machholdt <joga17ac@student.cbs.dk>
2021-03-19 15:08:57 +0100

    match

--- Model/matches.js
+++ Model/matches.js
@@ -1,3 +1,8 @@
 class Matches {
-    constructor(m)
+    constructor(id, user_id1, user_id2) {
+        this.id = id;
+        this.user_id1 = user_id1;
+        this.user_id2 = user_id2;
+    }
+    
 }
